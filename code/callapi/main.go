6b9cf03613a6be4e08154c021720105ec43fe524
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-01-22 05:39:51 +0000

    improves on call API example

--- code/callapi/main.go
+++ code/callapi/main.go
@@ -21,6 +21,19 @@ func main() {
 	// dump HTTP header info:
 	b, _ := httputil.DumpResponse(resp, false)
 	fmt.Print(string(b))
+	// dump response body
+	var buf [512]byte
+	reader := resp.Body
+	payload := 0
+	for {
+		n, err := reader.Read(buf[0:])
+		if err != nil {
+			break
+		}
+		payload += n
+		fmt.Print(string(buf[0:n]))
+	}
+	fmt.Printf("\n--DONE reading %d bytes\n", payload)
 }
 
 // ENDC OMIT
